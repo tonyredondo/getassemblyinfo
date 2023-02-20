5186295d3ccf7e9779d6363802d87bfcbb40193d
Tony Redondo <tony.redondo@datadoghq.com>
2023-02-20 11:07:55 +0100

    Compact mode and native export (#2)
    * Add compact mode and export a native function.
    
    * changes
    
    * publish shared
    
    * fixes
    
    * change

--- GetAssemblyInfo/Program.cs
+++ GetAssemblyInfo/Program.cs
@@ -1,79 +1,31 @@
-﻿using System.Reflection;
-using System.Text;
-using Mono.Cecil;
+﻿using GetAssemblyInfo;
 
-if (args?.Length < 1)
+class Program
 {
-    Console.WriteLine("You must specify a filename as an argument");
-    Environment.ExitCode = 1;
-    return;
-}
-
-var filePath = args?[0];
-if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
-{
-    Console.WriteLine("File '{0}' doesn't exist.", filePath);
-    Environment.ExitCode = 1;
-    return;
-}
-
-var assemblyName = AssemblyName.GetAssemblyName(filePath);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.Name), assemblyName.Name);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.Version), assemblyName.Version);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.Flags), assemblyName.Flags);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.CultureName), assemblyName.CultureName);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.FullName), assemblyName.FullName);
-Console.WriteLine("{0}: {1}", nameof(assemblyName.ContentType), assemblyName.ContentType);
-var processedAttributes = new List<string>();
-try
-{
-    var asm = AssemblyDefinition.ReadAssembly(filePath);
-    Console.WriteLine("ModuleVersionId: {0}", asm.MainModule.Mvid);
-    foreach (var attr in asm.MainModule.GetCustomAttributes())
+    public static void Main(string[] args)
     {
-        try
+        if (args.Length < 1)
         {
-            if (processedAttributes.Contains(attr.AttributeType.FullName))
-            {
-                continue;
-            }
-            
-            processedAttributes.Add(attr.AttributeType.FullName);
-
-            if (attr.HasProperties)
-            {
-                foreach (var prop in attr.Properties)
-                {
-                    Console.WriteLine("{0}.{1}: {2}", attr.AttributeType.Name, prop.Name, prop.Argument.Value);
-                }
-            }
-
-            if (attr.HasConstructorArguments)
-            {
-                var arguments = new List<string>();
-                for (var i = 0; i < attr.ConstructorArguments.Count; i++)
-                {
-                    var ctrArg = attr.Constructor.Parameters[i];
-                    if (string.IsNullOrEmpty(ctrArg.Name))
-                    {
-                        arguments.Add(attr.ConstructorArguments[i].Value?.ToString() ?? "(null)");
-                    }
-                    else
-                    {
-                        arguments.Add( ctrArg.Name + ": " + (attr.ConstructorArguments[i].Value?.ToString() ?? "(null)"));
-                    }
-                }
-                
-                Console.WriteLine("{0}: {1}", attr.AttributeType.Name, string.Join(", ", arguments));
-            }
+            Console.WriteLine("You must specify a filename as an argument");
+            Environment.ExitCode = 1;
+            return;
         }
-        catch
+
+        var filePath = args[0];
+        if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
         {
-            //
+            Console.WriteLine("File '{0}' doesn't exist.", filePath);
+            Environment.ExitCode = 1;
+            return;
+        }
+
+        if (args.Length > 1 && args[1] == "--full")
+        {
+            Console.Write(AssemblyInfo.GetFullAssemblyInfoFromFilePath(filePath));
+        }
+        else
+        {
+            Console.Write(AssemblyInfo.GetCompactAssemblyInfoFromFilePath(filePath));
         }
     }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
 }
